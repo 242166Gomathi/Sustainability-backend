a6de9c5e305206237e4b0de09a1ff762e8e82a30
242166Gomathi <160576855+242166Gomathi@users.noreply.github.com>
2024-09-03 20:47:52 +0530

    Delete sustainability-app directory

--- sustainability-app/src/main/java/com/project/sustainability/repository/ContactDetailsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.sustainability.repository;
-
-import com.project.sustainability.model.ContactDetails;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ContactDetailsRepository extends MongoRepository<ContactDetails, String> {
-}
