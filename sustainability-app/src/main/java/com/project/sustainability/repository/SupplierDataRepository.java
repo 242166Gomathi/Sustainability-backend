ad4916a1ba661b096711c2fca3a8738309ab25a2
242166Gomathi <160576855+242166Gomathi@users.noreply.github.com>
2024-09-03 20:20:44 +0530

    Delete sustainability-app directory

--- sustainability-app/src/main/java/com/project/sustainability/repository/SupplierDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.sustainability.repository;
-
-
-
-import com.project.sustainability.model.SupplierData;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SupplierDataRepository extends MongoRepository<SupplierData, String> {
-}
